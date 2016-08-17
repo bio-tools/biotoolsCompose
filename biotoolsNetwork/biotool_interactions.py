94d9d9bd4885dae8f3432d5b1f4d1b2881376322
Veit <veitveit@gmail.com>
2016-08-17 15:24:18 +0200

    /bin/bas 	modified:   biotoolsNetwork/biotool_interactions.py

--- biotoolsNetwork/biotool_interactions.py
+++ biotoolsNetwork/biotool_interactions.py
@@ -12,7 +12,11 @@ data = json.loads(response)
 
 idx2name = dict()      # Look-up the tool name by its index 
 input2tool = dict()    # Look-up all the tools that uses a particular EDAM input format
+input2tool2 = dict()    # Look-up all the tools that uses a particular EDAM input data type
+input2tool3 = dict()    # Look-up all the tools that uses a particular EDAM input data type | data format
 output2tool = dict()   # Look-up all the tools that uses a particular EDAM output format 
+output2tool2 = dict()   # Look-up all the tools that uses a particular EDAM output data type
+output2tool3 = dict()   # Look-up all the tools that uses a particular EDAM output data type | data format
 operations2tool = dict()     # Full EDAM operations for each tool
 
 # For all the tools:
@@ -28,22 +32,48 @@ for idx, tool in enumerate(data):
             # Now all the inputs of the tool function:
             if 'input' in function:
                 for f_input in function['input']:
-                    # Then all the data formats used for the input:
-                    if 'dataFormat' in f_input:
-                        for dataFormat in f_input['dataFormat']:
-                            # Extract and keep the EDAM url:
-                            if 'uri' in dataFormat:
-                                EDAM_url = dataFormat['uri']
-                                # Get the EDAM format id from the url:
-                                EDAM_id = EDAM_url.split('/')[-1]
-                                # Store the tool index for this EDAM input format:
-                                if EDAM_id in input2tool:
-                                    input2tool[EDAM_id].append(idx)
-                                else:
-                                    input2tool[EDAM_id] = [idx]
-                            else: # No url defined
+                    if 'dataType' in f_input:
+                        curr_type = ""
+                        # print(f_input['dataType'])
+                        dataType = f_input['dataType']
+                        # Extract and keep the EDAM url:
+                        if 'uri' in dataType:
+                            EDAM_url = dataType['uri']
+                            # Get the EDAM format id from the url:
+                            EDAM_id = EDAM_url.split('/')[-1]
+                            curr_type = EDAM_id
+                            # Store the tool index for this EDAM input format:
+                            if EDAM_id in input2tool2:
+                                input2tool2[EDAM_id].append(idx)
+                            else:
+                                input2tool2[EDAM_id] = [idx]
+                        else: # No url defined
+                            pass
+                        # Then all the data formats used for the input:
+                        if 'dataFormat' in f_input:
+                            for dataFormat in f_input['dataFormat']:
+                                # Extract and keep the EDAM url:
+                                if 'uri' in dataFormat:
+                                    EDAM_url = dataFormat['uri']
+                                    # Get the EDAM format id from the url:
+                                    EDAM_id = EDAM_url.split('/')[-1]
+                                    # Store the tool index for this EDAM input format:
+                                    combkey = curr_type + "|" + EDAM_id
+                                    print(combkey + " " + name)
+
+                                    if EDAM_id in input2tool:
+                                        input2tool[EDAM_id].append(idx)
+                                    else:
+                                        input2tool[EDAM_id] = [idx]
+                                    if combkey in input2tool3:
+                                        input2tool3[combkey].append(idx)
+                                    else:
+                                        input2tool3[combkey] = [idx]                                        
+                                else: # No url defined
+                                    pass
+                            else: # No dataFormat or EDAM_url defined
                                 pass
-                    else: # No dataFormat or EDAM_url defined
+                    else: # No dataType defined
                         pass
             else: # No input defined
                 pass
@@ -71,20 +101,51 @@ for idx, tool in enumerate(data):
                     
 
             # Same procedure as with the input defined above:
+    if 'function' in tool:
+        for function in tool['function']:
+            # Now all the inputs of the tool function:
             if 'output' in function:
                 for f_output in function['output']:
-                    if 'dataFormat' in f_output:
-                        for dataFormat in f_output['dataFormat']:
-                            if 'uri' in dataFormat:
-                                EDAM_url = dataFormat['uri']
-                                EDAM_id = EDAM_url.split('/')[-1]
-                                if EDAM_id in output2tool:
-                                    output2tool[EDAM_id].append(idx)
-                                else:
-                                    output2tool[EDAM_id] = [idx]
-                            else: # No url defined
+                    if 'dataType' in f_output:
+                        curr_type = ""
+                        dataType = f_output['dataType']
+                        # Extract and keep the EDAM url:
+                        if 'uri' in dataType:
+                            EDAM_url = dataType['uri']
+                            # Get the EDAM format id from the url:
+                            EDAM_id = EDAM_url.split('/')[-1]
+                            curr_type = EDAM_id
+                            # Store the tool index for this EDAM output format:
+                            if EDAM_id in output2tool2:
+                                output2tool2[EDAM_id].append(idx)
+                            else:
+                                output2tool2[EDAM_id] = [idx]
+                        else: # No url defined
+                            pass
+                        # Then all the data formats used for the input:
+                        if 'dataFormat' in f_output:
+                            for dataFormat in f_output['dataFormat']:
+                                # Extract and keep the EDAM url:
+                                if 'uri' in dataFormat:
+                                    EDAM_url = dataFormat['uri']
+                                    # Get the EDAM format id from the url:
+                                    EDAM_id = EDAM_url.split('/')[-1]
+                                    # Store the tool index for this EDAM input format:
+                                    combkey = curr_type + "|" + EDAM_id
+                                    # print(combkey + " " + name)
+                                    if EDAM_id in output2tool:
+                                        output2tool[EDAM_id].append(idx)
+                                    else:
+                                        output2tool[EDAM_id] = [idx]
+                                    if combkey in output2tool3:
+                                        output2tool3[combkey].append(idx)
+                                    else:
+                                        output2tool3[combkey] = [idx]                                        
+                                else: # No url defined
+                                    pass
+                            else: # No dataFormat or EDAM_url defined
                                 pass
-                    else: # No dataFormat defined
+                    else: # No dataType defined
                         pass
             else: # No output defined
                 pass
@@ -99,6 +160,13 @@ for EDAM_id, outtool_idx in output2tool.items():
 
 for EDAM_id, intool_idx in input2tool.items():
     input2tool[EDAM_id] = sorted(list(set(intool_idx)))
+
+for EDAM_id, outtool_idx in output2tool3.items():
+    output2tool3[EDAM_id] = sorted(list(set(outtool_idx)))
+
+for EDAM_id, intool_idx in input2tool3.items():
+    input2tool3[EDAM_id] = sorted(list(set(intool_idx)))
+
 
 
 # Find the tools with matching output and input:
@@ -129,8 +197,36 @@ for EDAM_id, outtool_idx in output2tool.items():
 else:
         pass
 
+# Find the tools with matching output and input:
+out_match_in3 = dict()
+# provides EDAM format for each pair of tools
+out_edam_in3 = dict()
+# Look through all the EDAM output formats:
+for EDAM_id, outtool_idx in output2tool3.items():
+    # Is the output format also existing as and input to another tool?:
+    if EDAM_id in input2tool3:
+        intool_idx = input2tool3[EDAM_id]
+        # Now make the connection between the list of input tools and each matching output tool:
+        for tool in outtool_idx:
+            if tool in out_match_in3:
+                # Remove dublicates on the fly, or the dict will grow huge:
+                out_match_in3[tool] = sorted(list(set(out_match_in3[tool] + intool_idx)))
+                for intool in intool_idx:
+                    ind = "_".join((str(tool),str(intool)))
+                    if ind in out_edam_in3:
+                        out_edam_in3[ind] = sorted(list(set(out_edam_in3[ind] + [EDAM_id])))
+                    else:
+                        out_edam_in3[ind] = [EDAM_id]
+            else:
+                out_match_in3[tool] = intool_idx
+                for intool in intool_idx:
+                    ind = "_".join((str(tool),str(intool)))
+                    out_edam_in3[ind] = [EDAM_id]
+else:
+        pass
+   
 
-# Print the results to a Cytoscape tab separated interaction file:
+# Print the results to a Cytoscape tab separated interaction files (1. combined entry per matching EDAM format, 2. individual entries):
 with open("biotools.interactions", 'w') as file_handle:
     # Print header:
     print('{0}\t{1}\t{2}\t{3}'.format('tool name/source node', 'tool name/target node', 'interaction format', 'source EDAM operations', 'target EDAM operations'), end="\n", file=file_handle)
@@ -146,4 +242,38 @@ with open("biotools.interactions", 'w') as file_handle:
             in_operations = operations2tool[in_tool]
             edam_name = out_edam_in["_".join((str(out_tool),str(in_tool)))]
             print('{0}\t{1}\t{2}\t{3}\t{4}'.format(out_name, edam_name,  in_name, out_operations, in_operations), end="\n", file=file_handle)
-        
+
+with open("biotools.interactions.full", 'w') as file_handle:
+    # Print header:
+    print('{0}\t{1}\t{2}\t{3}'.format('tool name/source node', 'tool name/target node', 'interaction format', 'source EDAM operations', 'target EDAM operations'), end="\n", file=file_handle)
+    # Go through all tools with output matching an input:
+    for out_tool, in_list in out_match_in.items():
+        # Convert the output tool index to its name:
+        out_name = idx2name[out_tool]
+        out_operations = operations2tool[out_tool]
+        # Print each interaction as one line in the file:
+        for in_tool in in_list:
+            # Convert the input tool index to its name:
+            in_name = idx2name[in_tool]
+            in_operations = operations2tool[in_tool]
+            edam_name = out_edam_in["_".join((str(out_tool),str(in_tool)))]
+            for edam_fo in edam_name:
+                print('{0}\t{1}\t{2}\t{3}\t{4}'.format(out_name, edam_fo,  in_name, out_operations, in_operations), end="\n", file=file_handle)
+
+with open("biotools.interactions.combEDAM", 'w') as file_handle:
+    # Print header:
+    print('{0}\t{1}\t{2}\t{3}'.format('tool name/source node', 'interaction format', 'tool name/target node', 'source EDAM operations', 'target EDAM operations'), end="\n", file=file_handle)
+    # Go through all tools with output matching an input:
+    for out_tool, in_list in out_match_in3.items():
+        # Convert the output tool index to its name:
+        out_name = idx2name[out_tool]
+        out_operations = operations2tool[out_tool]
+        # Print each interaction as one line in the file:
+        for in_tool in in_list:
+            # Convert the input tool index to its name:
+            in_name = idx2name[in_tool]
+            in_operations = operations2tool[in_tool]
+            edam_name = out_edam_in3["_".join((str(out_tool),str(in_tool)))]
+            for edam_fo in edam_name:
+                print('{0}\t{1}\t{2}\t{3}\t{4}'.format(out_name, edam_fo,  in_name, out_operations, in_operations), end="\n", file=file_handle)
+
